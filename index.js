13e83c059e4aea57862f7d2dc2e293941e787a35
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-14 16:42:03 +0200

    ex. 5 to fix

---
+++ index.js
@@ -0,0 +1,16 @@
+const delBtn1 = document.getElementById("delBtn1");
+const delBtn2 = document.getElementById("delBtn2");
+const delBtn3 = document.getElementById("delBtn3");
+const delBtn4 = document.getElementById("delBtn4");
+const delBtn5 = document.getElementById("delBtn5");
+const ulTrackList1 = document.querySelectorAll("div#trackList>ul#ultrack1")
+console.log(ulTrackList1)
+const ulTrackList2 = document.querySelector("div#trackList>ul#ultrack2")
+const ulTrackList3 = document.querySelector("div#trackList>ul#ultrack3")
+const ulTrackList4 = document.querySelector("div#trackList>ul#ultrack4")
+const ulTrackList5 = document.querySelector("div#trackList>ul#ultrack5")
+
+const delTrack = function(ulId) {
+    ulId.innerText = "";
+}
+delBtn1.addEventListener("cllick", delTrack(ulTrackList1))
