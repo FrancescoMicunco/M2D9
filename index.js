9ab4faefe5bd380002c8d8a154a588122aa45e47
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-14 23:30:34 +0200

    ex 7

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const delBtn1 = document.getElementById("delBtn1");
-const delBtn2 = document.getElementById("delBtn2");
-const delBtn3 = document.getElementById("delBtn3");
-const delBtn4 = document.getElementById("delBtn4");
-const delBtn5 = document.getElementById("delBtn5");
+const delBtn = document.querySelectorAll("delBtn");
+const addBtn = document.getElementById("addBtn");
+const modalDiv = document.getElementById("modalDiv");
+
 const ulTrackList1 = document.querySelectorAll("div#trackList>ul#ultrack1")
 console.log(delBtn1)
 
-/* const ulTrackList2 = document.querySelector("div#trackList>ul#ultrack2")
-const ulTrackList3 = document.querySelector("div#trackList>ul#ultrack3")
-const ulTrackList4 = document.querySelector("div#trackList>ul#ultrack4")
-const ulTrackList5 = document.querySelector("div#trackList>ul#ultrack5")
- */
-const delTrack = function(ulId) {
-    ulId.innerText = "";
-}
-delBtn1.addEventListener("click", function(e) {
+// ex 6
+delBtn.addEventListener("click", function(e) {
     let button = e.target;
     button.parentNode.parentNode.removeChild(parentNode);
 })
+
+// ex 7
